5a001faf80d011733e13edab3041242415b950d2
DESKTOP-F955D5N\dangb <buitandang96@gmail.com>
2018-03-11 22:38:32 +0700

    build client

---
+++ client/components/App/index.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+export default class App extends Component {
+  constructor() {
+
+  }
+  render(){
+    return (
+      <div> App component</div>
+    )
+  }
+}
