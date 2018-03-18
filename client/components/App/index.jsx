956672b00e34edec27f07d9b722205ff573d414a
DESKTOP-F955D5N\dangb <buitandang96@gmail.com>
2018-03-18 23:31:18 +0700

    deploy mern to heroku

--- client/components/App/index.jsx
+++ client/components/App/index.jsx
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
 
   render(){
     return (
-      <div> App componentaaaa</div>
+      <div>
+        <h1> App component</h1>
+        <button >Click me</button>
+        <Link to='/home'>home</Link>
+      </div>
+
     )
   }
 }
--- client/router.jsx
+++ client/router.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import App from './components/App/index.jsx';
+import Home from './components/Home/index.jsx';
 
 const routes = (
   <Switch>
     <Route exact path='/' component={App}/>
+    <Route exact  path='/home' component={Home}/>
   </Switch>
 )
 export default routes
