55db9b5d678228aa55ae1757fa81cd03932352e9
DESKTOP-F955D5N\dangb <buitandang96@gmail.com>
2018-06-23 11:28:02 +0700

    fix PORT ENV

--- client/index.jsx
+++ client/index.jsx
@@ -8,11 +8,11 @@ import 'react-notifications/lib/notifications.css';
 import './index.scss';
 
 if (process.env.NODE_ENV === 'production') {
-  console.log('Welcome to production');
+  console.log(process.env.NODE_ENV);
 } else {console.log('Welcome to develop'); }
 if (process.env.PORT == 'http://localhost:3000') {
   console.log('Debugging output');
-} else {console.log('http://localhost:3000')}
+} else {console.log(process.env.PORT)}
 import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory({basename: '/', forceRefresh: false});
